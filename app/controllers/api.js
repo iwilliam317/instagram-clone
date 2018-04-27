b1bec115f1fc7e44fb50f0ba09c96712be8e6ca7
william <willshinji@gmail.com>
2018-04-27 01:12:21 -0300

    converting prototype to class

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -5,20 +5,5 @@ module.exports._get = (app, req, res) => {
   let api = new app.app.models.api(connection)
   // res.json('asddas')
    api._get(res);
-  // app.get('/api', (req, res) => {
-    // db.open((error, mongoclient) => {
-    //   mongoclient.collection('postagens', (error, collection) => {
-    //     collection.find().toArray((error, result) => {
-    //       if(error){
-    //         res.json(error)
-    //       }
-    //       else{
-    //         res.json(result);
-    //       }
 
-    //       mongoclient.close();
-    //     });
-    //   })
-    // })
-  // })
 }
--- app/models/api.js
+++ app/models/api.js
@@ -1,24 +1,25 @@
-function Api (connection){
-  this._connection = connection()
-}
+class Api {
+  constructor(connection){
+    this._connection = connection();
+  }
 
-Api.prototype._get = function(res){
+  _get(res){
+      this._connection.open((error, mongoclient) => {
+        mongoclient.collection('postagens', (error, collection) => {
+                collection.find().toArray((error, result) => {
+                  if(error){
+                    res.json(error)
+                  }
+                  else{
+                    res.json(result);
+                  }
 
-  this._connection.open((error, mongoclient) => {
-    mongoclient.collection('postagens', (error, collection) => {
-            collection.find().toArray((error, result) => {
-              if(error){
-                res.json(error)
-              }
-              else{
-                res.json(result);
-              }
+                  mongoclient.close();
+                });
+              })
+      })
 
-              mongoclient.close();
-            });
-          })
-  })
-
+  }
 }
 
 module.exports = () => {
