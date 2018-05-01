4c97709677c1f15b8d61f90a7b26d934e93a5b08
william <willshinji@gmail.com>
2018-05-01 19:01:42 -0300

    refactors spec

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -4,31 +4,36 @@ const base_url = "http://localhost:3000/";
 
 describe('routes', () => {
 
-  let server;
+    let server;
 
-  beforeAll(() => {
-    server = require('../app');
-  });
-
-  afterAll(() => {
-    server.close();
-  });
-
-  describe('GET /', () => {
-    it('returns status code 200 ', (done) => {
-      request.get(base_url, (error, response, body) => {
-        expect(response.statusCode).toEqual(200);
-        done();
-      });
+    beforeAll(() => {
+        server = require('../app');
     });
 
-    it('returns ', (done)=> {
-      request.get(base_url, (error, response, body) => {
-        expect(body).toEqual('{"msg":"hello, welcome!"}');
-        done();
-      })
+    afterAll(() => {
+        server.close();
     });
 
-  });
+    describe('GET /', () => {
+
+        let data = {};
+
+        beforeAll((done) => {
+            request.get(base_url, (error, response, body) => {
+                data.statusCode = response.statusCode;
+                data.body = body;
+                done();
+            });
+        });
+
+        it('returns status code 200 ', () => {      
+            expect(data.statusCode).toEqual(200);
+        });
+
+        it('returns ', ()=> {
+            expect(data.body).toEqual('{"msg":"hello, welcome!"}');
+        });
+
+    });
 
 });
