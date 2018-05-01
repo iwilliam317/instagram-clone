26fb2f2ebef8e50534cd4d56443de91da137d69b
william <willshinji@gmail.com>
2018-05-01 13:56:46 -0300

    removes comments

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,28 +1,5 @@
 module.exports = (app) => {
 
-
-    // const db = app.config.dbConnection();
-    // POST
-    // app.post('/api', (req, res) => {
-    //   let data = req.body;
-
-    //   db.open((error, mongoclient) => {
-    //     mongoclient.collection('postagens', (error, collection) => {
-    //       collection.insert(data, (error, result) => {
-    //         if (error){
-    //           res.json({'status': 'Error'});
-    //         }
-    //         else{
-
-    //           res.json({'status': 'Ok'});
-    //         }
-
-    //         mongoclient.close();
-    //       });
-    //     })
-    //   })
-    // })
-
     app.post('/api', (req, res) => {
       let data = req.body;
       app.app.controllers.api.methodPost(app, req, res, data)
