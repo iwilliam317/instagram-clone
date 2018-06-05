7c869cb40ab3970e2ae6c2a714f8fb739d92b02e
william <willshinji@gmail.com>
2018-06-05 12:38:13 -0300

    sends comment id back to view

--- api/app/controllers/api.js
+++ api/app/controllers/api.js
@@ -46,7 +46,7 @@ module.exports.methodShow = async (application, request, response) => {
 }
 
 module.exports.methodPut = async (application, request, response) => {
-    response.send('api put')
+    response.send(request.params.id);
     // const connection = application.config.dbConnection; 
     // let api = await new application.app.models.api(connection);
     // let id = request.params.id;
